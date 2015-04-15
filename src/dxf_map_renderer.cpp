c78ba79912adef7bc1ccb1ffe703f21e6b123dc8
Richard Hanten <richard.hanten@uni-tuebingen.de>
2015-04-15 23:30:41 +0200

    minor optimization

--- src/dxf_map_renderer.cpp
+++ src/dxf_map_renderer.cpp
@@ -126,60 +126,62 @@ void run(const std::string &map,
     dxf_map.getPolygon(polygon, dxf::DXFMap::getLayerAttribFilter("valid_area"));
     std::cout << polygon.outer().size() << std::endl;
 
-    /// outer ring
-    cv::Scalar color = randomColor(rng);
-    unsigned int outer_size = polygon.outer().size();
-    for(unsigned int i = 0 ; i < outer_size ; ++i) {
-        dxf::DXFMap::Point start = polygon.outer().at(i % outer_size);
-        dxf::DXFMap::Point end   = polygon.outer().at((i + 1) % outer_size);
-        cv::Point2d cstart = transformToCV(min, resolution, start);
-        cv::Point2d cend   = transformToCV(min, resolution, end);
-        cv::line(mat_polygons, cstart, cend, color, 1, CV_AA);
-    }
+    if(!polygon.outer().empty()) {
 
-    /// inner rings
-    color = randomColor(rng);
-    for(unsigned int i = 0 ; i < polygon.inners().size() ; ++i) {
-        dxf::DXFMap::Polygon::ring_type &ring = polygon.inners().at(i);
-        unsigned int ring_size = ring.size();
-        for(unsigned int j = 0 ; j < ring_size ; ++j) {
-            dxf::DXFMap::Point start = ring.at(j % ring_size);
-            dxf::DXFMap::Point end   = ring.at((j + 1) % ring_size);
+        /// outer ring
+        cv::Scalar color = randomColor(rng);
+        unsigned int outer_size = polygon.outer().size();
+        for(unsigned int i = 0 ; i < outer_size ; ++i) {
+            dxf::DXFMap::Point start = polygon.outer().at(i % outer_size);
+            dxf::DXFMap::Point end   = polygon.outer().at((i + 1) % outer_size);
             cv::Point2d cstart = transformToCV(min, resolution, start);
             cv::Point2d cend   = transformToCV(min, resolution, end);
             cv::line(mat_polygons, cstart, cend, color, 1, CV_AA);
         }
-    }
 
-    cv::Point2d pos(-1.0, -1.0);
-    cv::setMouseCallback("map", mouseEvent, &pos);
-
-
-    cv::Mat mat_polygons_buff = mat_polygons.clone();
-    cv::flip(mat_polygons_buff, mat_polygons_buff, 0);
-    while(true) {
-        if(pos.x > 0.0 && pos.y > 0.0) {
-            pos.y = mat_polygons_buff.rows - pos.y;
-
-            mat_polygons_buff = mat_polygons.clone();
-            dxf::DXFMap::Point dp = transfomrToDXF(min, resolution, pos);
-            cv::Scalar color(0, 0, 255);
-            if(boost::geometry::within(dp, polygon)) {
-                color = cv::Scalar(0, 255, 0);
+        /// inner rings
+        color = randomColor(rng);
+        for(unsigned int i = 0 ; i < polygon.inners().size() ; ++i) {
+            dxf::DXFMap::Polygon::ring_type &ring = polygon.inners().at(i);
+            unsigned int ring_size = ring.size();
+            for(unsigned int j = 0 ; j < ring_size ; ++j) {
+                dxf::DXFMap::Point start = ring.at(j % ring_size);
+                dxf::DXFMap::Point end   = ring.at((j + 1) % ring_size);
+                cv::Point2d cstart = transformToCV(min, resolution, start);
+                cv::Point2d cend   = transformToCV(min, resolution, end);
+                cv::line(mat_polygons, cstart, cend, color, 1, CV_AA);
             }
-
-            cv::circle(mat_polygons_buff, pos, 2, color, CV_FILLED, CV_AA);
-            pos.x = -1.0;
-            pos.y = -1.0;
-            cv::flip(mat_polygons_buff, mat_polygons_buff, 0);
         }
 
-        cv::imshow("map", mat_polygons_buff);
-        int key = cv::waitKey(19) & 0xFF;
-        if(key == 27)
-            break;
-    }
+        cv::Point2d pos(-1.0, -1.0);
+        cv::setMouseCallback("map", mouseEvent, &pos);
 
+
+        cv::Mat mat_polygons_buff = mat_polygons.clone();
+        cv::flip(mat_polygons_buff, mat_polygons_buff, 0);
+        while(true) {
+            if(pos.x > 0.0 && pos.y > 0.0) {
+                pos.y = mat_polygons_buff.rows - pos.y;
+
+                mat_polygons_buff = mat_polygons.clone();
+                dxf::DXFMap::Point dp = transfomrToDXF(min, resolution, pos);
+                cv::Scalar color(0, 0, 255);
+                if(boost::geometry::within(dp, polygon)) {
+                    color = cv::Scalar(0, 255, 0);
+                }
+
+                cv::circle(mat_polygons_buff, pos, 2, color, CV_FILLED, CV_AA);
+                pos.x = -1.0;
+                pos.y = -1.0;
+                cv::flip(mat_polygons_buff, mat_polygons_buff, 0);
+            }
+
+            cv::imshow("map", mat_polygons_buff);
+            int key = cv::waitKey(19) & 0xFF;
+            if(key == 27)
+                break;
+        }
+    }
     std::cout << "Have a nice day!" << std::endl;
 }
 }
