e42cd1e90ad627aa86dafe555ccd7c0479c0d779
Richard Hanten <richard.hanten@uni-tuebingen.de>
2017-01-10 15:28:45 +0100

    cmake

--- cmake/optional_dependencies.cmake
+++
@@ -1,6 +0,0 @@
-find_package(OpenCV QUIET)
-find_package(roscpp QUIET)
-find_package(gazebo QUIET)
-find_package(xacro  QUIET)
-find_package(Qt5 COMPONENTS Core Gui Widgets QUIET)
-find_package(Eigen3 QUIET)
